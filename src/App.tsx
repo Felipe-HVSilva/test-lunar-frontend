672cc4dba37f5f6be77f2b75e0c7b143364229be
Felipe-HVSilva <felype.hen@hotmail.com>
2024-06-03 04:44:48 -0300

    feat: add Products List

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,54 @@
+import { Search } from "lucide-react";
 import Header from "./_components/Header";
+import ProductItem from "./_components/ProductItem";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [products, setProducts] = useState<[]>([]);
+
+  async function LoadProduct() {
+    const response = await fetch(
+      "https://mks-frontend-challenge-04811e8151e6.herokuapp.com/api/v1/products?page=1&rows=10&sortBy=id&orderBy=DESC"
+    );
+
+    const data = await response.json();
+    console.log(data);
+
+    setProducts(data.products);
+  }
+
+  useEffect(() => {
+    LoadProduct();
+  }, []);
+
   return (
-    <div>
+    <div className="h-screen">
       <Header />
+      <div className="bg-slate-50 w-full h-full flex py-5 px-5 items-start justify-center">
+        <main className="max-w-5xl w-full">
+          <div className=" flex justify-between gap-3 mb-7 max-sm:flex-col">
+            <div className="flex items-center justify-between rounded-lg bg-white px-2 py-3">
+              <input
+                type="text"
+                placeholder="Nome"
+                className="bg-inherit focus:outline-none"
+              />
+              <Search size={21} />
+            </div>
+
+            <select className="px-2 py-3 rounded-lg">
+              <option value="50">R$ 0 - R$ 50</option>
+              <option value="100">R$ 51,00 - R$ 100,00</option>
+            </select>
+          </div>
+
+          <div className="grid grid-cols-4 grid-rows-2 gap-x-5 gap-y-7 max-md:grid-cols-1 place-items-center ">
+            {products.map((product) => (
+              <ProductItem product={product} />
+            ))}
+          </div>
+        </main>
+      </div>
     </div>
   );
 }
