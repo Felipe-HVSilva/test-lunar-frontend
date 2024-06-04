522a1eae776646330289637e9878d5adfdc301a0
Felipe-HVSilva <felype.hen@hotmail.com>
2024-06-04 14:44:46 -0300

    fix: ensure button add cart is clicked

--- src/_components/ProductItem/index.tsx
+++ src/_components/ProductItem/index.tsx
@@ -21,31 +21,35 @@ const ProductItem = ({ product }: ProductProps) => {
   }
 
   return (
-    <Link
-      to={`product/${product.name}`}
-      className="max-w-[300px] min-h-[285px] rounded bg-white flex flex-col "
-    >
-      <div className="max-w-[100px] max-h-[115px] m-auto">
-        <img src={product.photo} alt="" />
-      </div>
-
-      <div className="flex flex-col px-3 mt-2">
-        <div className="flex items-center justify-between">
-          <h3 className="font-medium max-w-28 line-clamp-1">{product.name}</h3>
-          <span className="bg-black text-white font-bold px-1 py-2 rounded">
-            {Intl.NumberFormat("pt-br", {
-              style: "currency",
-              currency: "BRL",
-            }).format(product.price)}
-          </span>
+    <main>
+      <Link
+        to={`product/${product.name}`}
+        className="max-w-[300px] min-h-[250px] rounded bg-white flex flex-col "
+      >
+        <div className="max-w-[100px] max-h-[115px] m-auto">
+          <img src={product.photo} alt="" />
         </div>
 
-        <div className="mt-2">
-          <p className="font-light text-[10px] line-clamp-3">
-            {product.description}
-          </p>
+        <div className="flex flex-col px-3 mt-2">
+          <div className="flex items-center justify-between">
+            <h3 className="font-medium max-w-28 line-clamp-1">
+              {product.name}
+            </h3>
+            <span className="bg-black text-white font-bold px-1 py-2 rounded">
+              {Intl.NumberFormat("pt-br", {
+                style: "currency",
+                currency: "BRL",
+              }).format(product.price)}
+            </span>
+          </div>
+
+          <div className="mt-2">
+            <p className="font-light text-[10px] line-clamp-3">
+              {product.description}
+            </p>
+          </div>
         </div>
-      </div>
+      </Link>
 
       <button
         className="bg-purple-700 py-1 w-full rounded-b text-white mt-2"
@@ -53,7 +57,7 @@ const ProductItem = ({ product }: ProductProps) => {
       >
         Adicionar
       </button>
-    </Link>
+    </main>
   );
 };
 
